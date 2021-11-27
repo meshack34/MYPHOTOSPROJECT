c40a3d0b48bc034ac79fc2728731afd4fcd9a2ac
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-11-27 11:24:33 +0300

    add search model

--- photos/models.py
+++ photos/models.py
@@ -43,3 +43,9 @@ class Article(models.Model):
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
         return news
+        
+       #..search
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
