5576b721b8a3396c26afb5b43d544a83850290f9
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-11-27 11:22:59 +0300

    add search url

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     url(r'^$', views.news_today, name = 'newsToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews') 
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+     url(r'^search/', views.search_results, name='search_results')
 ]
