4211730659ca50d1cc669b410c6cdc60aa2b3cf5
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-11-27 10:45:19 +0300

    update admin file

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import Editor,Article,tags
+
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+admin.site.register(Editor)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(tags)
