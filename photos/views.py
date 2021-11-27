5cd840864997f51519b75e217adcbaedcc5bb4b0
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-11-27 10:53:52 +0300

    add view photos

--- photos/views.py
+++ photos/views.py
@@ -1,16 +1,14 @@
+
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Article
 
-# Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
-
-def news_of_day(request):
+# Create views
+def news_today(request):
     date = dt.date.today()
-    return render(request, 'my-photos/daily-photos.html',{'date': date})
-
-
+    news = Article.todays_news()
+    return render(request, 'my-photos/daily-photos.html',{'date': date,"news":news})
 
 def past_days_news(request,past_date):
     
@@ -23,6 +21,7 @@ def past_days_news(request,past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
-
-    return render(request, 'my-photos/my-gallery.html', {"date":date})
+        return redirect(news_today)
+    
+    news = Article.days_news(date)
+    return render(request, 'my-photos/my-gallery.html', {"date":date,"news":news})
