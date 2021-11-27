8421c4f18b127a7e2b0ef1e5a1671f629fe4e9cf
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-11-27 09:06:15 +0300

    add app url

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http  import HttpResponse
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
